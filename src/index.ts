04f43b7561bc983a832feb14d269dda0135d02e6
Amanda Custodio <amandajcustodio@outlook.com>
2025-07-02 12:19:52 -0300

    Added page not found middleware

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import { routes } from "./routes/index"
 import { initializeApp } from 'firebase-admin/app';
 import { errorHandler } from "./middlewares/error-handler.middleware";
+import { pageNotFoundHandler } from "./middlewares/page-not-found.middleware";
 
 initializeApp();
 const app = express();
 
 routes(app);
+pageNotFoundHandler(app);
 errorHandler(app);
 
 app.listen(3000);

53399f9a341d1344d8648e1eda11b2c110c9cf89
Amanda Custodio <amandajcustodio@outlook.com>
2025-07-02 10:44:18 -0300

    Added try catch to methods and error 500 return with middleware

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express"
+import { NextFunction, Request, Response } from "express"
 import { getFirestore } from "firebase-admin/firestore"
 
 type User = {
@@ -11,57 +11,77 @@ const collection: string = "users";
 
 export class UsersController {
   
-  public static async getAll(req: Request, res: Response) {
-    const snapshot = await getFirestore().collection(collection).get();
+  public static async getAll(req: Request, res: Response, next: NextFunction) {
+    try {
+      const snapshot = await getFirestore().collection(collection).get();
 
-    const users = snapshot.docs.map(doc => {
-      return {
+      const users = snapshot.docs.map(doc => {
+        return {
+          id: doc.id,
+          ...doc.data()
+        } as User;
+      });
+
+      res.status(201).send(users);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public static async getById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const userId = req.params.id;
+      const doc = await getFirestore().collection(collection).doc(userId).get();
+    
+      res.status(201).send({
         id: doc.id,
         ...doc.data()
-      } as User;
-    });
-
-    res.send(users);
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 
-  public static async getById(req: Request, res: Response) {
-    const userId = req.params.id;
-    const doc = await getFirestore().collection(collection).doc(userId).get();
-  
-    res.send({
-      id: doc.id,
-      ...doc.data()
-    });
+  public static async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const user = req.body;
+
+      await getFirestore().collection(collection).add(user);
+
+      res.status(204).send({
+        message: "Usuário criado com sucesso!"
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 
-  public static async create(req: Request, res: Response) {
-    const user = req.body;
+  public static async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const userId = req.params.id;
+      const newUser = req.body as User;
 
-    await getFirestore().collection(collection).add(user);
-
-    res.send({
-      message: "Usuário criado com sucesso!"
-    });
-  }
-
-  public static async update(req: Request, res: Response) {
-    const userId = req.params.id;
-    const newUser = req.body as User;
-
-    await getFirestore().collection(collection).doc(userId).set(newUser)
-  
-    res.send({
-      message: "Usuário atualizado com sucesso!"
-    });
-  }
-
-  public static async delete(req: Request, res: Response) {
-    const userId = req.params.id;
+      await getFirestore().collection(collection).doc(userId).set(newUser)
     
-    await getFirestore().collection(collection).doc(userId).delete();
+      res.status(204).send({
+        message: "Usuário atualizado com sucesso!"
+      });
+    } catch (error) {
+      next(error)
+    }
+  }
 
-    res.send({
-      message: "Usuário deletado com sucesso!"
-    })
+  public static async delete(req: Request, res: Response, next: NextFunction) {
+    try {
+      const userId = req.params.id;
+    
+      await getFirestore().collection(collection).doc(userId).delete();
+
+      res.status(204).end({
+        message: "Usuário deletado com sucesso!"
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { routes } from "./routes/index"
 import { initializeApp } from 'firebase-admin/app';
 
 initializeApp();
 
 const app = express();
+
 routes(app);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).send({
+    message: "Erro interno do servidor!"
+  });
+});
 
 app.listen(3000);

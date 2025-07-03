fd66e119e1d21ba31971ffb5caf40d463223cde3
Amanda Custodio <amandajcustodio@outlook.com>
2025-07-03 11:35:59 -0300

    Correction: receive ids and User object insted of Request from controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express"
 import { UserService } from "../services/user.service";
+import { User } from "../models/user.model";
 
 export class UsersController {
   
@@ -8,11 +9,11 @@ export class UsersController {
   }
 
   public static async getById(req: Request, res: Response, next: NextFunction) {
-    res.status(200).send(await new UserService().getById(req));
+    res.status(200).send(await new UserService().getById(req.params.id));
   }
 
   public static async create(req: Request, res: Response, next: NextFunction) {
-    await new UserService().create(req);
+    await new UserService().create(req.body);
 
     res.status(201).send({
       message: "Usuário criado com sucesso!"
@@ -20,7 +21,10 @@ export class UsersController {
   }
 
   public static async update(req: Request, res: Response, next: NextFunction) {
-    await new UserService().update(req);
+    const userId = req.params.id;
+    const reqBody = req.body as User;
+
+    await new UserService().update(userId, reqBody);
   
     res.status(200).send({
       message: "Usuário atualizado com sucesso!"
@@ -28,7 +32,7 @@ export class UsersController {
   }
 
   public static async delete(req: Request, res: Response, next: NextFunction) {
-    await new UserService().delete(req);
+    await new UserService().delete(req.params.id);
     res.status(204).end();
   }
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,6 @@
 import { getFirestore } from "firebase-admin/firestore";
 import { User } from "../models/user.model";
 import { NotFoundError } from "../errors/not-found.error";
-import { Request } from "express";
 
 export class UserService {
   private readonly collection = "users";
@@ -17,8 +16,7 @@ export class UserService {
     });
   }
 
-  public async getById(req: Request): Promise<User> {
-    const userId = req.params.id;
+  public async getById(userId: string): Promise<User> {
     const doc = await getFirestore().collection(this.collection).doc(userId).get();
 
     if (!doc.exists) {
@@ -31,15 +29,12 @@ export class UserService {
     } as User;
   }
 
-  public async create(req: Request): Promise<void> {
-    const user = req.body;
+  public async create(user: User): Promise<void> {
     await getFirestore().collection(this.collection).add(user);
   }
 
-  public async update(req: Request): Promise<void> {
-    const userId = req.params.id;
+  public async update(userId: string, user: User): Promise<void> {
     const docRef = await getFirestore().collection(this.collection).doc(userId);
-    const reqBody = req.body as User;
     const currUser = await docRef.get();
 
     if (!currUser.exists) {
@@ -48,14 +43,13 @@ export class UserService {
 
     const newUser: User = {
       ...currUser.data(),
-      ...reqBody
+      ...user
     }
 
     await docRef.set(newUser)
   }
 
-  public async delete(req: Request): Promise<void> {
-    const userId = req.params.id;
+  public async delete(userId: string): Promise<void> {
     const docRef = await getFirestore().collection(this.collection).doc(userId);
     const user = await docRef.get();
 

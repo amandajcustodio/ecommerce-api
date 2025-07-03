f5e9cde50b2a09b41f22a6c02408440248ec45fd
Amanda Custodio <amandajcustodio@outlook.com>
2025-07-03 09:55:07 -0300

    Added userSchema to celebrate validation

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import { UsersController } from "../controllers/user.controller";
 import asyncHandler from "express-async-handler";
-import { celebrate, Joi, Segments } from "celebrate";
+import { celebrate, Segments } from "celebrate";
+import { userSchema } from "../models/user.model";
 
 export const usersRoute = express.Router();
 usersRoute.use(express.json());
@@ -10,13 +11,8 @@ usersRoute.get("/users", asyncHandler(UsersController.getAll));
 
 usersRoute.get("/users/:id", asyncHandler(UsersController.getById));
 
-usersRoute.post("/users", celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().required(),
-    email: Joi.string().email().required()
-  })
-}), asyncHandler(UsersController.create));
+usersRoute.post("/users", celebrate({ [Segments.BODY]: userSchema }), asyncHandler(UsersController.create));
 
-usersRoute.put("/users/:id", asyncHandler(UsersController.update));
+usersRoute.put("/users/:id", celebrate({ [Segments.BODY]: userSchema }), asyncHandler(UsersController.update));
 
 usersRoute.delete("/users/:id", asyncHandler(UsersController.delete));

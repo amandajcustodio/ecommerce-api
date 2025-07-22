370d2a5f86388700cb696e3ce9806165a8034da5
Amanda Custodio <amandajcustodio@outlook.com>
2025-07-22 20:17:13 -0300

    feat(firebase app): installed and configurate firebase/app

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,13 @@ import { routes } from "./routes/index"
 import { initializeApp } from 'firebase-admin/app';
 import { errorHandler } from "./middlewares/error-handler.middleware";
 import { pageNotFoundHandler } from "./middlewares/page-not-found.middleware";
+import { initializeApp as initializeFirebaseApp } from "firebase/app";
 
 initializeApp();
+initializeFirebaseApp({
+  apiKey: process.env.API_KEY
+});
+
 const app = express();
 
 routes(app);
